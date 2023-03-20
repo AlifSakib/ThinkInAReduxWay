1eb08f9bc21b66d89e7e265d59c354353a7e5f48
AlifSakib <alifsakib@gmail.com>
2023-03-20 21:42:16 +0600

    added store

--- Module - 02/2.1 Setup React Redux/think-in-a-redux-way/src/App.js
+++ Module - 02/2.1 Setup React Redux/think-in-a-redux-way/src/App.js
@@ -1,15 +1,19 @@
+import { Provider } from "react-redux";
 import Counter from "./components/Counter";
+import store from "./redux/store";
 
 export default function App() {
   return (
-    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
-        Simple Counter Application
-      </h1>
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-    </div>
+    <Provider store={store}>
+      <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+        <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+          Simple Counter Application
+        </h1>
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+      </div>
+    </Provider>
   );
 }
